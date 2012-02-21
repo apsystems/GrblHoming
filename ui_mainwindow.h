4f0c22b5c72167c4a2a11d2fa93ee69234970916
Enrique Condes <kosmerebel@gmail.com>
2012-02-20 18:45:35 -0600

    first release

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Mon 20. Feb 18:42:52 2012
+** Created: Mon 20. Feb 18:45:15 2012
 **      by: Qt User Interface Compiler version 4.8.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
